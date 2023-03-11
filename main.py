77f36226843db3552f0bb5f68cde4071e1e7df2c
gennadynemchin@gmail.com <gennadynemchin@gmail.com>
2023-03-11 21:12:17 +0300

    Added create_product func to elasticpath.py;

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import json
 
 
-with open("addresses.json", "r") as addresses:
+with open("jsons/addresses.json", "r") as addresses:
     addresses = addresses.read()
 
-with open("menu.json", "r") as menu:
+with open("jsons/menu.json", "r") as menu:
     menu_pizza = menu.read()
 
 addresses = json.loads(addresses)
 menu = json.loads(menu_pizza)
-print(addresses)
-print(menu_pizza)
+
+
+for element in menu:
+    name = element['name']
+    slug = 123
+    sku = 456
+    description = element['description']
+
+    print(name, slug, sku, description)
